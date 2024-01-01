50d578e1a3840c12bda3a54b3d1c5b390426f503
GNUS <60074926+theMark001@users.noreply.github.com>
2024-01-01 16:14:05 +0100

    update cod

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,14 @@
-import TeleBot from "telebot"
+import TeleBot from "telebot";
+import mongo from "./db.mjs";
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on('text', (msg) => (msg.text.startsWith('/') ? null : msg.reply.text(msg.text)));
 
-export default bot
+bot.on('start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
+
+bot.on('/env', (msg) => msg.reply.text(process.env.VERCEL_ENV || 'VERCEL_ENV is not defined'));
+
+bot.on('/db', (msg) => msg.reply.text(mongo.db().databaseName));
+
+export default bot;
