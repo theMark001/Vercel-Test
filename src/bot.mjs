7c21f60b586b6a40d6531d44ff34b6f264752fb1
GNUS <60074926+theMark001@users.noreply.github.com>
2024-01-01 16:35:57 +0100

    hope

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,7 +3,7 @@ import mongo from "./db.mjs";
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on('text', (msg) => (msg.text.startsWith('/') ? null : msg.reply.text(msg.text)));
+bot.on("text", msg => msg.reply.text(msg.text))
 
 bot.on('start', (msg) => msg.reply.photo('https://picsum.photos/1000'));
 
